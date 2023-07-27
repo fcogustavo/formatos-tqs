4f6fd0693a6021e18807215fcf76b35303d2744b
Francisco Gustavo <gustavocmdb@gmail.com>
2023-07-27 09:50:18 -0300

    Update arquives

--- node_cbe25fe37d78a.tmp.js
+++ node_cbe25fe37d78a.tmp.js
@@ -35,59 +35,38 @@ const pdfDataExtract = async (file) => {
 		let numbers = [];
 		textSplit.forEach(txt => {
 			let a = txt.split(' ')
-			
+			console.log(a)
 			if (Number(a[0]) > 0) {
-				if (a.length == 3) {
-					console.log(a)
+				let i = 1
+				b = a[1]
+				while (Number(b.slice(b.length - i, b.length)) <= Number(a[0])) {
+					i++
 				}
-				
-				if (a.length == 4) {
-					let i = 1
-					let b = a[1]
-					while (Number(b.slice(b.length - i, b.length)) <= Number(a[0])) {
-						i++
+				let c = a[1].slice(0, a[1].length - i)
+				let d = a[1].slice(a[1].length - i, a[1].length)
+				a.splice(1, 1, c, d)
+				console.log(a)
+				numbers.push(
+					{
+						formato: Number(a[0]),
+						dobras: Number(a[1])
 					}
-					let c = a[1].slice(0, a[1].length - i)
-					if (c === '') {
-						c = undefined
+				);
+				numbers.push(
+					{
+						formato: Number(a[2]),
+						dobras: Number(a[3])
 					}
-					let d = a[1].slice(a[1].length - i, a[1].length)
-					a.splice(1, 1, c, d)
-					i = 1
-					b = a[3]
-					while (Number(b.slice(b.length - i, b.length)) <= Number(a[2])) {
-						i++
-					}
-					c = a[3].slice(0, a[3].length - i)
-					if (c === '') {
-						c = undefined
-					}
-					d = a[3].slice(a[3].length - i, a[3].length)
-					a.splice(3, 1, c, d)
-					//console.log(a)
-					numbers.push(
-						{
-							formato: Number(a[0]),
-							dobras: Number(a[1])
-						}
-					);
-					numbers.push(
-						{
-							formato: Number(a[2]),
-							dobras: Number(a[3])
-						}
-					);
-					numbers.push(
-						{
-							formato: Number(a[4]),
-							dobras: Number(a[5])
-						}
-					)
-				}
+				)
 			}
 
 		})
-		return {numbers};
+
+
+
+		
+
+		return {gauges, summary, totalWeight};
 	} catch (err) {
 		console.log(`Ocorreu um erro durante o processo: ${err.message}`);
 		return "Ocorreu um erro. Verifique o formato do arquivo.";
